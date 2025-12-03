bf392ab37c729d8a77098b398a46e68f004054f8
Padmavathi Subramaniyan <psubramaniyan@logitech.com>
2025-12-03 12:52:34 +0530

    Initial commit

---
+++ xcode-cloud-testapp/xcode-cloud-testappTests/xcode_cloud_testappTests.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloud_testappTests.swift
+//  xcode-cloud-testappTests
+//
+//  Created by Padmavathi Subramaniyan on 03/12/25.
+//
+
+import Testing
+@testable import xcode_cloud_testapp
+
+struct xcode_cloud_testappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ xcode-cloud-testapp/xcode-cloud-testappUITests/xcode_cloud_testappUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  xcode_cloud_testappUITestsLaunchTests.swift
+//  xcode-cloud-testappUITests
+//
+//  Created by Padmavathi Subramaniyan on 03/12/25.
+//
+
+import XCTest
+
+final class xcode_cloud_testappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
